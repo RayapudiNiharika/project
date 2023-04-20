95754fd0d845668bdd470455d4025473f47c44f3
Gagan Chander Pandey <104014249+Gagan-1804@users.noreply.github.com>
2023-04-20 13:53:49 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login';
-import Title from './components/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/login/Login";
+import Title from "./components/header/Header";
 // import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
 
 function App() {
   return (
-    <div >
-      <Title/>
+    <div>
+      <Title />
       <Login />
     </div>
   );
