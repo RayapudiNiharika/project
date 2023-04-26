d53482e2dbe4ddfd3909e846366e7a67a0f539d9
Gagan Chander Pandey <104014249+Gagan-1804@users.noreply.github.com>
2023-04-26 15:35:40 +0530

    Ui update 2 (4 cards added)

--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -123,6 +123,25 @@ const Welcome = () => {
           <div className="col-md-3">
             <div class="card">
               <img
+                class="image card-img-top"
+                src="https://images.pexels.com/photos/6044812/pexels-photo-6044812.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <h5 class="card-title">About Us</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div class="card">
+              <img
                 class="card-img-top"
                 src="https://images.pexels.com/photos/221164/pexels-photo-221164.jpeg?auto=compress&cs=tinysrgb&w=600"
                 alt="Card image cap"
